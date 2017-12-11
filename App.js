8147bc3b1746f1749be07deaa1feee1dc6fcd88c
Bhavya Sruthi Sode <sbhavyasruthi@gmail.com>
2017-12-11 10:40:12 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import Expo from "expo";
+import React from "react";
+import App from "./js/AppMain";
+
+export default class App1 extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+        isReady: false
+        };
+    }
+    
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+                                  Roboto: require("native-base/Fonts/Roboto.ttf"),
+                                  Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+                                  Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+                                  });
+        
+        this.setState({ isReady: true });
+    }
+    render() {
+        if (!this.state.isReady) {
+            return <Expo.AppLoading />;
+        }
+        return <App />;
+    }
+}
